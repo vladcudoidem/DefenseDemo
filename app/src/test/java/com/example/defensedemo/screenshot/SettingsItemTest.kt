5aabe6c915f9388ef9c3e8f3a9126c338061eeaf
Vlad Vamos <vladvamos@protonmail.com>
2025-03-25 00:45:33 +0100

    add roborazzi stage

---
+++ app/src/test/java/com/example/defensedemo/screenshot/SettingsItemTest.kt
@@ -0,0 +1,29 @@
+package com.example.defensedemo.screenshot
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.defensedemo.SettingItem
+import com.example.defensedemo.ui.theme.DefenseDemoTheme
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+class SettingsItemTest {
+    @get:Rule
+    val roborazziRule = ScreenshotUtil.customRule
+
+    @Test
+    fun capture() {
+        captureRoboImage {
+            DefenseDemoTheme {
+                SettingItem(
+                    title = "Wi-Fi",
+                    description = "Manage the Wi-Fi connections."
+                )
+            }
+        }
+    }
+}
